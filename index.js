e7bc69a7331aae6566c364a071e6fe743f1f03f8
Santi <santiago.38@hotmail.com>
2025-11-04 22:17:50 -0300

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import "./src/db/mongoose.js";
 import restaurantsRouter from "./src/routes/restaurants.route.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(cors({
+  origin: '*', // change this to your local app URL (e.g., React dev server)
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+}));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
